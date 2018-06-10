578a05972517e190ff15ccb324987ae258eaff4a
Didier Roche <didrocks@ubuntu.com>
2018-06-10 10:57:16 +0200

    Temporary binary to play with the internal BB library

---
+++ cmd/moveyourmartin/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/didrocks/wiiboard-sphero-game/internal/wiiboard"
+)
+
+func main() {
+	board, err := wiiboard.Detect()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	battery, err := board.Battery()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(battery)
+	go board.Listen()
+	for event := range board.Events {
+		fmt.Printf("%+v\n", event)
+	}
+}
